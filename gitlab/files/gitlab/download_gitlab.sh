1b794fe782aaa9c4fcad801c227ed72b094d351f
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2016-07-03 02:27:50 +0900

    update script

--- gitlab/files/gitlab/download_gitlab.sh
+++ gitlab/files/gitlab/download_gitlab.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-wget https://packages.gitlab.com/gitlab/gitlab-ce/packages/el/7/gitlab-ce-8.9.3-ce.0.el7.x86_64.rpm/download
+RPM=gitlab-ce-8.9.3-ce.0.el7.x86_64.rpm
+
+wget https://packages.gitlab.com/gitlab/gitlab-ce/packages/el/7/${RPM}/download \
+-O ${RPM}
